0df2fb7ea440ee40986a5e3e0cb07c8b4a6e1d04
Trevor Jameson <github email address>
2018-11-18 18:37:37 -0500

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -7,17 +7,28 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    author: '',
+    content: ''
   }
 
   handleOnChange = event => {
     // Handle Updating Component State
+    const {value, name} = event.target
+    this.setState({[name]: value})
   }
 
   handleOnSubmit = event => {
     // Handle Form Submit event default
+    event.preventDefault()
     // Create quote object from state
+    const quote = {...this.state, id: uuid()}
     // Pass quote object to action creator
+    this.props.addQuote(quote)
     // Update component state to return to default state
+    this.setState({
+      content: '',
+      author: ''
+    })
   }
 
   render() {
@@ -34,6 +45,8 @@ class QuoteForm extends Component {
                       <textarea
                         className="form-control"
                         value={this.state.content}
+                        name="content"
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -44,6 +57,8 @@ class QuoteForm extends Component {
                         className="form-control"
                         type="text"
                         value={this.state.author}
+                        name="author"
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -63,4 +78,4 @@ class QuoteForm extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, {addQuote})(QuoteForm);
